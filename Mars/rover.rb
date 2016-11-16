a4f23f7188fbfe917801dbb62c0336abf2787c3d
Yuan He <yuanthehe@gmail.com>
2016-11-16 15:05:31 -0500

    not finished

---
+++ Mars/rover.rb
@@ -0,0 +1,38 @@
+class Rover
+  def initialize(x, y, dir)
+  @x = x
+  @y = y
+  @dir = direction
+
+
+    def instruction
+
+    end
+
+
+    def turn
+
+    end
+
+
+    def move
+
+    end
+
+  end
+end
+
+puts "what is the boundaries of the plateou?"
+boundaries = gets.chomp.split
+
+puts "What is the starting coordinate of the 1st Rover?"
+coordinate1 = gets.chomp.split
+
+puts "Please give the 1st Rover's instruction below:"
+instructions1 = gets.chomp.split
+
+puts "What is the starting position of the 2nd Rover?"
+coordinate2 = gets.chomp.split
+
+puts "Please give the 2nd Rover's instruction below:"
+instructions2 = gets.chomp.split
---
+++ paperboy/paperboy.rb
@@ -0,0 +1,12 @@
+class Paperboy
+
+  def initialize(name, experience)
+    @name = name
+    @experience = experience
+
+  end
+
+  @
+
+
+end
